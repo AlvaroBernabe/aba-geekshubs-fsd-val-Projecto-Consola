8cab94aea57664b36ba2bb1d8fafd59b7f8b8cd4
alvarito101093 <alvaro101093@gmail.com>
2023-10-10 10:16:13 +0200

    feat: Added brightness level to brightness button

--- js/main.js
+++ js/main.js
@@ -5,4 +5,31 @@ function iniciarIntro() {
     var lcd = document.querySelector(".lcd");
     lcd.innerHTML = "";
     lcd.appendChild(video);
+};
+
+document.getElementById('brillo').addEventListener('click', cambiarBrillo);
+let nivelBrillo = 0;
+
+function cambiarBrillo() {
+    const lcd = document.querySelector('.lcd');
+    
+    nivelBrillo++;
+    if (nivelBrillo > 3) {
+        nivelBrillo = 1;
+    }
+
+    switch (nivelBrillo) {
+        case 1:
+            lcd.style.filter = "brightness(100%)";
+            break;
+        case 2:
+            lcd.style.filter = "brightness(150%)";
+            break;
+        case 3:
+            lcd.style.filter = "brightness(200%)";
+            break;
+        default:
+            lcd.style.filter = "brightness(100%)";
+            break;
+    }
 }
