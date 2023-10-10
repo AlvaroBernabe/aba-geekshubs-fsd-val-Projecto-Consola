f8d27cc30d6530347ded9bd2c5e8ae68aa971b4a
alvarito101093 <alvaro101093@gmail.com>
2023-10-10 18:03:41 +0200

    Fix 2: JavaScript Intro URL

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function iniciarIntro() {
     var video = document.createElement("video");
-    video.src = "media/intro.mp4";
+    video.src = "./intro.mp4";
     video.autoplay = true; 
     var lcd = document.querySelector(".lcd");
     lcd.innerHTML = "";
